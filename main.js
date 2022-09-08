20e5839f4236aa327f61373d0b403d3373147071
Martin Silva <martinsilva510@gmail.com>
2022-09-07 23:53:26 -0300

    proyecto final

--- main.js
+++ main.js
@@ -1,16 +1,122 @@
-function Productos(codigoProductos, detalleProductos, costoProductos, stockProductos){
-this.codigo = codigoProductos;
-this.detalle = detalleProductos;
-this.costo = costoProductos;
-this.stock = stockProductos;
 
-}
+//  const account= {
+//     nombre : "martin",
+//     card : "411411411411",
+//     amount : 1000,
+
+//     retiro(quantity) {
+//         this.amount= this.amount - (quantity)
+//     }
+//  }
+
+
+// account.retiro(200)
+// account.retiro(100)
+// account.retiro(200)
+// account.retiro(100)
+// console.log(account)
  
-let remeraHombre = new Productos(000, "remera0 para Hombre", 900, 20);
-let remeraMujer = new Productos(111, "remera para Mujer",1500, 5);
-let camperaHombre = new Productos(222, "campera para Hombre",4000, 6);
-let camperaMujer = new Productos(333, "campera para Mujer", 3500,10);
-let camisetaFutbol = new Productos(444, "camiseta de futbol", 7000, 12);
-let camisetaFutbolchicos = new Productos(555, "camiseta de futbol para chicos", 3000, 8);
+//funcion flecha
+//const numero = (num1, num2) =>  (num1 * num2)
 
-const todosLosProductos = [remeraHombre, remeraMujer, camperaHombre, camperaMujer, camisetaFutbol, camisetaFutbolchicos];
+//const resultado = numero(20, 2)
+//console.log(resultado)
+
+// const card = document.querySelector('.card')
+// const cardImg = document.querySelector('.cardImg')
+// const cardTitle = document.querySelector('.cardTitle')
+// const price = document.querySelector('.price')
+
+
+const contGrid = document.querySelector('.contGrid')
+const tablaCarr = document.querySelector('.tablaCarr')
+ 
+try{fetch('api.json')
+.then(respuesta =>{
+    return respuesta.json()
+} )
+.then( datos => {
+    let contador = 0;
+while (datos.length > contador) {
+
+let card = document.createElement('div')
+
+let imgCont = document.createElement('div')
+let img = document.createElement ('img')
+
+let cardBody = document.createElement('div')
+
+let title = document.createElement('h5')
+let price = document.createElement('p')
+
+let addCarr = document.createElement('button')
+
+
+card.setAttribute('class','card')
+
+imgCont.setAttribute('class','imgCont')
+img.setAttribute('src',datos[contador].imagen)
+img.setAttribute('alt', 'imagen de representacion')
+
+cardBody.setAttribute('class','cardBody')
+
+title.innerHTML = datos[contador].nombre;
+price.innerHTML = datos[contador].precio + '$';
+
+title.setAttribute('class','cardTitle')
+price.setAttribute('class', 'price')
+
+addCarr.setAttribute('class','btn btn-dark')
+addCarr.setAttribute('value',contador)
+addCarr.innerHTML = 'añadir al carrito'
+
+
+card.appendChild(imgCont)
+imgCont.appendChild(img)
+card.appendChild(cardBody)
+cardBody.appendChild(title)
+cardBody.appendChild(price)
+cardBody.appendChild(addCarr)
+
+contGrid.appendChild(card)
+
+ 
+addCarr.addEventListener('click', (e) => {
+
+const tr = document.createElement('tr')
+const tdUno = document.createElement('td')
+const tdDos = document.createElement('td')
+
+
+tdUno.innerHTML = datos[addCarr.value].nombre
+tdDos.innerHTML = datos [addCarr.value].precio
+
+
+
+   tr.appendChild(tdUno);
+   tr.appendChild(tdDos);
+   tablaCarr.appendChild(tr);
+
+});
+
+ 
+    contador++;
+   
+}
+
+  
+})
+} catch (e) {
+    console.log(e);
+}
+
+
+
+
+
+
+
+
+
+
+
